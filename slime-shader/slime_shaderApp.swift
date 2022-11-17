ed42b9fb01cfd7dc8c0beeec19f7e2abc6898911
Nitesh Kumar Niranjan <hello@theux.dev>
2022-11-18 02:08:49 +0530

    Initial Commit

---
+++ slime-shader/slime_shaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  slime_shaderApp.swift
+//  slime-shader
+//
+//  Created by Nitesh Kumar Niranjan on 18/11/22.
+//
+
+import SwiftUI
+
+@main
+struct slime_shaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
